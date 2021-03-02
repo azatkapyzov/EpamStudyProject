274df9b06d5e41f273e032b0108bc35ea811de3c
azatkapyzov <kapyzovazat@gmail.com>
2021-03-02 23:42:24 +0600

    Add MainTask

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,87 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String lastName;
+    private String name;
+    private String patronymic;
+    private String address;
+    private String creditCardNumber;
+    private String bankAccountNumber;
+
+    public Customer (int id, String lastName, String name, String patronymic, String address, String creditCardNumber, String bankAccountNumber) {
+        this.id = id;
+        this.lastName = lastName;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.creditCardNumber = creditCardNumber;
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public Customer (int id, String creditCardNumber) {
+        this.id = id;
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+}
